d12e8a084df1eceea54eb8cd1ae4fbcbc9360696
Mohitpatil05 <patilmohit00089@gmail.com>
2025-07-27 13:27:21 +0530

    Initial commit

--- App.js
+++ App.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { enableScreens } from 'react-native-screens';
 import AppNavigator from './src/navigation/AppNavigator';
-import { Platform, SafeAreaView, StatusBar } from 'react-native';
+import { LogBox, Platform, SafeAreaView, StatusBar } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+
+AntDesign.loadFont();
+MaterialCommunityIcons.loadFont();
+Feather.loadFont();
+MaterialIcons.loadFont();
+Octicons.loadFont();
+Foundation.loadFont();
+Ionicons.loadFont();
+Entypo.loadFont();
+FontAwesome.loadFont();
+
+LogBox.ignoreAllLogs();
 enableScreens();
 
 export default function App() {
